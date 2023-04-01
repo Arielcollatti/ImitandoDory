c54cc2020367082bda8b285625f5b5014922f4c6
Rick Anjo <colatti2017@gmail.com>
2023-03-31 21:22:06 -0300

    exercicioresolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("--imitandodory--/n");
+
+Console.Write("digite uma frase...: ");
+string frase = Console.ReadLine()!;
+
+string fraseEmBaleies = frase
+  .Replace("a", "aaa")
+  .Replace("A", "AAA")
+  .Replace("e", "eee")
+  .Replace("E", "EEE")
+  .Replace("i", "iii")
+  .Replace("I", "III")
+  .Replace("o", "ooo")
+  .Replace("O", "OOO")
+  .Replace("u", "uuu")
+  .Replace("U", "UUU");
+
+  Console.WriteLine($"\nFrase em baleies: {fraseEmBaleies}");
